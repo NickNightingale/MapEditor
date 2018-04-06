4d67a34794f285c4d0382237064ab263dc97e26b
Nick Nightingale <nick@nightingale.to>
2018-04-06 14:33:08 +0100

    Started creating models for the user interface

---
+++ ViewModels/Map.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditor.ViewModels
+{
+	public class Map : ViewModelBase
+	{
+		private ObservableCollection<Tile> _tiles;
+
+		public ObservableCollection<Tile> Tiles
+		{
+			get
+			{
+				if (_tiles == null)
+				{
+					Tiles = new ObservableCollection<Tile>();
+				}
+				return _tiles;
+			}
+			set
+			{
+				if (_tiles != value)
+				{
+					_tiles = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+	}
+}
---
+++ ViewModels/Tile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditor.ViewModels
+{
+	public class Tile : ViewModelBase
+	{
+		private int _gridX;
+		public int GridX
+		{
+			get { return _gridX; }
+			set
+			{
+				if (_gridX != value)
+				{
+					_gridX = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+		private int _gridY;
+		public int GridY
+		{
+			get { return _gridY; }
+			set
+			{
+				if (_gridY != value)
+				{
+					_gridY = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+	}
+}
